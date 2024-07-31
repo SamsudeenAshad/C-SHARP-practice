ae919781fde875f22106fab47fdc3cf723b5604b
Sim Shad <samsudeenashad@icloud.com>
2024-07-31 10:02:13 +0530

    C# project create
    C# project create

---
+++ ClassDiagramUni/ClassDiagramUni/Department.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDiagramUni
+{
+    public class Department
+    {
+        public string name { get; set; }
+        public string code { get; set; }
+
+        public Department(string Name, string code)
+        {
+            this.name = Name;
+            this.code = code;
+        }
+
+        public void addCourse(Course course)
+        {
+            course.Add(course);
+        }
+    }
+}
---
+++ ClassDiagramUni/ClassDiagramUni/University.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDiagramUni
+{
+    public class University
+    {
+        public string name { get; set; }
+        public string location { get; set; }
+        public University( String Name, String Location ) 
+        {
+            Name = name;
+            Location = location;
+        }
+
+        public void addDepartment(Department dep)
+        {
+            department.add(dep);
+        }
+
+        public List<Department>departments { get; set; }
+
+        public List<Department> GetDepartments()
+        {
+        }
+
+        University uni1 = new University("NSBM", "Homagama");
+    }
+}
